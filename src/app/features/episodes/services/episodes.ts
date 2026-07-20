252bc123415a8402b22ea0acc013b5454a4467ec
Kimberly Moreau <moreau.magali.kimberly@gmail.com>
2026-07-20 17:18:23 +0200

    feat: :label: Update episodes service
    Add two functions for API

--- src/app/features/episodes/services/episodes.ts
+++ src/app/features/episodes/services/episodes.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Episode } from '../types/episodes.type';
+import { ApiResponse } from '../../../shared/types/api-response.types';
 
 @Injectable({
   providedIn: 'root',
 })
 
-export class Episodes {
+export class EpisodesService {
+  private readonly http = inject(HttpClient);
+  private episodes = signal<Episode[]>([]);
+  readonly characterSignal = this.episodes.asReadonly();
   readonly url = 'https://rickandmortyapi.com/api/episode/';
+
+    getEpisodesFromService(page: number = 1): Observable<ApiResponse<Episode[]>> {
+      return this.http
+        .get<ApiResponse<Episode[]>>(this.url, {
+          params: { page: page },
+        })
+        .pipe(tap((response: ApiResponse<Episode[]>) => this.episodes.set(response.results)));
+    }
+
+    getEpisodeFromComponent(page: number = 1): Observable<ApiResponse<Episode[]>> {
+      return this.http.get<ApiResponse<Episode[]>>(this.url, {
+        params: { page: page },
+      });
+    }
 }
