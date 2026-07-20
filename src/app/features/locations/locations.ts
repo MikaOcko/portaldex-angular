742b64b40ef908372ed30a19893f8abd7e20f6ea
Kimberly Moreau <moreau.magali.kimberly@gmail.com>
2026-07-20 16:19:16 +0200

    feat: Create new components (locations, episodes), updates main routes into app.routes.ts, import router link into components, update path constants

---
+++ src/app/features/locations/locations.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-locations',
+  imports: [RouterLink],
+  templateUrl: './locations.html',
+  styleUrl: './locations.css',
+})
+export class Locations {
+
+}
