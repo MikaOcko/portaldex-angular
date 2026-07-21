445dd62dbb3310796caddd22cfe2d6f0ce8fc5bd
Kimberly Moreau <moreau.magali.kimberly@gmail.com>
2026-07-21 08:44:30 +0200

    feat: :sparkles: Add new components (card, pagination) for episodes

---
+++ src/app/features/episodes/components/episode-card/episode-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-episode-card',
+  imports: [],
+  templateUrl: './episode-card.html',
+  styleUrl: './episode-card.css',
+})
+export class EpisodeCard {
+
+}
