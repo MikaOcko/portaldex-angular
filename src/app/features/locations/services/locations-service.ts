94e04ed22b020481b32be4db7cc5c56b46a66c10
Kimberly Moreau <moreau.magali.kimberly@gmail.com>
2026-07-21 10:46:22 +0200

    feat: :sparkles: Create type and service for locations (API call)

---
+++ src/app/features/locations/services/locations-service.ts
@@ -0,0 +1,30 @@
+import { Injectable, inject, signal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Location } from '../types/locations.type';
+import { ApiResponse } from '../../../shared/types/api-response.types';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class LocationsService {
+	private readonly http = inject(HttpClient);
+	private locations = signal<Location[]>([]);
+	readonly locationSignal = this.locations.asReadonly();
+	readonly url = 'https://rickandmortyapi.com/api/location';
+
+    getLocationsFromService(page: number = 1): Observable<ApiResponse<Location[]>> {
+      return this.http
+        .get<ApiResponse<Location[]>>(this.url, {
+          params: { page: page },
+        })
+        .pipe(tap((response: ApiResponse<Location[]>) => this.locations.set(response.results)));
+    }
+
+    getLocationFromComponent(page: number = 1): Observable<ApiResponse<Location[]>> {
+      return this.http.get<ApiResponse<Location[]>>(this.url, {
+        params: { page: page },
+      });
+    }
+}
